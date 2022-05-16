2290b5a29b3b782fe1d66817845c91ad07c82b58
Vishakh Aswani Kumar <vishakh@vishakhs-mbp.lan>
2022-05-15 21:20:00 -0500

    contact page with emailjs

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,23 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 import Footer from "../components/Footer";
-import { useState } from "react";
+import emailjs from "@emailjs/browser";
 import { useNavigate } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import "../styles/contactPage.css";
 const Contact = () => {
-    const [form, setForm] = useState({ name: "", email: "", message: "" });
-    const encode = (data) => {
-        return Object.keys(data)
-            .map(
-                (key) =>
-                    encodeURIComponent(key) +
-                    "=" +
-                    encodeURIComponent(data[key])
-            )
-            .join("&");
-    };
+    const form = useRef();
     //for redirect
     const navigate = useNavigate();
     const handleRedirect = () => {
@@ -26,19 +16,24 @@ const Contact = () => {
 
     // form submission
     const handleSubmit = (e) => {
-        fetch("/", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "contact", ...form }),
-        })
-            .then(() => handleRedirect())
-            .catch((error) => alert(error));
         e.preventDefault();
+        emailjs
+            .sendForm(
+                "service_cbv96ag",
+                "template_8avk1i5",
+                form.current,
+                "cXMKyMky8K9cT3wWo"
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            )
+            .then(handleRedirect());
     };
-    const handleChange = (event) => {
-        setForm({ ...form, [event.target.name]: event.target.value });
-    };
-
     return (
         <>
             <Container>
@@ -53,64 +48,56 @@ const Contact = () => {
                     </a>
                 </div>
                 <p>Or</p>
-                <form
-                    name="Contact"
-                    onSubmit={handleSubmit}
-                    method="POST"
-                    data-netlify="true"
-                >
+                <form ref={form} onSubmit={handleSubmit}>
                     <label>
                         Name:
                         <input
                             type="text"
-                            onChange={handleChange}
                             name="name"
                             className="input"
+                            required
                         />
                     </label>
                     <label>
                         Email:
                         <input
                             type="email"
-                            onChange={handleChange}
                             name="email"
                             className="input"
+                            required
                         />
                     </label>
                     <label>
                         Services Required:
                         <input
                             type="text"
-                            onChange={handleChange}
                             name="service-required"
                             className="input"
+                            required
                         />
                     </label>
                     <label>
                         Date From:
-                        <input
-                            type="date"
-                            onChange={handleChange}
-                            name="date-from"
-                            className="input"
-                        />
+                        <input type="date" name="date-from" className="input" />
                     </label>
                     <label>
                         Date To:
-                        <input
-                            type="date"
-                            onChange={handleChange}
-                            name="date-to"
-                            className="input"
-                        />
+                        <input type="date" name="date-to" className="input" />
                     </label>
                     <label>
                         Event Time:
                         <input
                             type="text"
-                            onChange={handleChange}
                             name="event-time"
                             className="input"
+                        />
+                    </label>
+                    <label>
+                        Further Queries:
+                        <textarea
+                            name="questions"
+                            row="4"
+                            className="textarea"
                         />
                     </label>
                     <button type="submit">Submit</button>
--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -1,7 +1,26 @@
 import React from "react";
-
+import makeuplogo from "../assets/makeup-logo.png";
+import { Link } from "react-router-dom";
+import "../styles/thankyou.css";
 const ThankYou = () => {
-    return <div>ThankYou for Submission</div>;
+    return (
+        <>
+            <p className="thankyou">Thank You!</p>
+            <p className="thankyou-for">For Your Inquiry</p>
+            <p className="return-main-page">
+                Click here to return to the main page
+            </p>
+            <Link to="/">
+                <div className="makeuplogo">
+                    <img
+                        className="makeuplogo1"
+                        src={makeuplogo}
+                        alt="makeuplogo"
+                    />
+                </div>
+            </Link>
+        </>
+    );
 };
 
 export default ThankYou;
